fa5b0794073b1cd930734390a34c078c9c9ea6c1
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 16:50:27 -0300

    :sparkles: feat: garante que seja retornado erro 400 se a confirmação de senha não for fornecida

--- unbook/backend/src/modules/accounts/controllers/signup.ts
+++ unbook/backend/src/modules/accounts/controllers/signup.ts
@@ -4,7 +4,12 @@ import { IHttpRequest, IHttpResponse } from "../protocols/http";
 
 class SignUpController {
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFields = ["name", "email", "password"];
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "passwordConfirmation",
+    ];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
