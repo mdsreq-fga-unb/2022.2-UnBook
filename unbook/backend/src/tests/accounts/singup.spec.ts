990687f78372118638b5cd2f82b17e6fbaed58be
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 17:51:39 -0300

    :test_tube: test: garante que o SignUpController chama o EmailValidator com o email correto

--- unbook/backend/src/tests/accounts/singup.spec.ts
+++ unbook/backend/src/tests/accounts/singup.spec.ts
@@ -97,4 +97,21 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParamError("email"));
   });
+
+  test("Deve chamar o EmailValidator com o email correto", () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest
+      .spyOn(emailValidatorStub, "isValid")
+      .mockReturnValueOnce(false);
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
+  });
 });
