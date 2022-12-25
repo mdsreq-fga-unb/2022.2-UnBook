393048ca3ce42fba2da09c699873c25d36cb8049
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 17:42:31 -0300

    :sparkles: feat: garante que seja retornado erro 400 se um email inválido for fornecido

--- unbook/backend/src/modules/accounts/controllers/signup.ts
+++ unbook/backend/src/modules/accounts/controllers/signup.ts
@@ -1,9 +1,17 @@
+import { InvalidParamError } from "../errors/invalid-param-error";
 import { MissingParamError } from "../errors/missing-param-error";
 import { badRequest } from "../helpers/http-helper";
 import { IController } from "../protocols/controller";
+import { IEmailValidator } from "../protocols/email-validator";
 import { IHttpRequest, IHttpResponse } from "../protocols/http";
 
 class SignUpController implements IController {
+  private readonly emailValidator: IEmailValidator;
+
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: IHttpRequest): IHttpResponse {
     const requiredFields = [
       "name",
@@ -16,7 +24,10 @@ class SignUpController implements IController {
         return badRequest(new MissingParamError(field));
       }
     }
-
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError("email"));
+    }
     return {
       statusCode: 200,
       body: {},
