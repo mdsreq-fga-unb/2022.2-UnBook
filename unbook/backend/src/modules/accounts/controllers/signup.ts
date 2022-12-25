3e8acd7c85bd3123989c438b577d9aef77829ac1
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 13:12:03 -0300

    :sparkles: feat: garante que seja retornado status 400 se o email não for fornecido

--- unbook/backend/src/modules/accounts/controllers/signup.ts
+++ unbook/backend/src/modules/accounts/controllers/signup.ts
@@ -1,8 +1,19 @@
 class SignUpController {
   handle(httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name"),
+      };
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: email"),
+      };
+    }
     return {
-      statusCode: 400,
-      body: new Error("Missing param: name"),
+      statusCode: 200,
     };
   }
 }
