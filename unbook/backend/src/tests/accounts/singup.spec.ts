4e702a696a50c281447bd55e4c5b7014d886bad7
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 18:37:26 -0300

    :recycle: refactor: move o EmailValidator para um helper method

--- unbook/backend/src/tests/accounts/singup.spec.ts
+++ unbook/backend/src/tests/accounts/singup.spec.ts
@@ -13,13 +13,26 @@ interface ISutTypes {
   emailValidatorStub: IEmailValidator;
 }
 
-const makeSut = (): ISutTypes => {
+const makeEmailValidator = (): IEmailValidator => {
   class EmailValidatorStub implements IEmailValidator {
     isValid(email: string): boolean {
       return true;
     }
   }
-  const emailValidatorStub = new EmailValidatorStub();
+  return new EmailValidatorStub();
+};
+
+const makeEmailValidatorWithError = (): IEmailValidator => {
+  class EmailValidatorStub implements IEmailValidator {
+    isValid(email: string): boolean {
+      throw new Error();
+    }
+  }
+  return new EmailValidatorStub();
+};
+
+const makeSut = (): ISutTypes => {
+  const emailValidatorStub = makeEmailValidator();
   const sut = new SignUpController(emailValidatorStub);
   return {
     sut,
@@ -120,12 +133,7 @@ describe("SignUp Controller", () => {
   });
 
   test("Deve retornar 500 se o EmailValidator retornar um erro", () => {
-    class EmailValidatorStub implements IEmailValidator {
-      isValid(email: string): boolean {
-        throw new ServerError();
-      }
-    }
-    const emailValidatorStub = new EmailValidatorStub();
+    const emailValidatorStub = makeEmailValidatorWithError();
     const sut = new SignUpController(emailValidatorStub);
     const httpRequest = {
       body: {
