f9f682cd5b8e6b9b8cb1d04a8cd8ca23936c248f
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 16:47:30 -0300

    :test_tube: test: garante que seja retornado erro 400 se a senha não for fornecida

--- unbook/backend/src/tests/accounts/singup.spec.ts
+++ unbook/backend/src/tests/accounts/singup.spec.ts
@@ -29,4 +29,18 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
   });
+
+  test("Deve retornar 400 se a senha não for enviada", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
+  });
 });
