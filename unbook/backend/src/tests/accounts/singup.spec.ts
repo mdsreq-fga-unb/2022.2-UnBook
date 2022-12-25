f2be3e76507486fd9ed38353fa1422d6aa2bc89f
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 19:53:57 -0300

    :test_tube: test: garante que a senha e a confirmação das senhas sejam iguais

--- unbook/backend/src/tests/accounts/singup.spec.ts
+++ unbook/backend/src/tests/accounts/singup.spec.ts
@@ -73,6 +73,23 @@ describe("SignUp Controller", () => {
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
 
+  test("Deve retornar 400 se a confirmação da senha e a senha não coincidirem", () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "invalid_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError("passwordConfirmation")
+    );
+  });
+
   test("Deve retornar 400 se a confirmação da senha não for enviada", () => {
     const { sut } = makeSut();
     const httpRequest = {
