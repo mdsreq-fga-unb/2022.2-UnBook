c713d2e9784e57704d5f922cd7723a205d7230c1
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 18:06:13 -0300

    :sparkles: feat: garante que o SignUpCOntroller retorna 500 se o EmailValidator retornar um erro

--- unbook/backend/src/modules/accounts/controllers/signup.ts
+++ unbook/backend/src/modules/accounts/controllers/signup.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from "../errors/invalid-param-error";
 import { MissingParamError } from "../errors/missing-param-error";
+import { ServerError } from "../errors/server-errors";
 import { badRequest } from "../helpers/http-helper";
 import { IController } from "../protocols/controller";
 import { IEmailValidator } from "../protocols/email-validator";
@@ -13,25 +14,32 @@ class SignUpController implements IController {
   }
 
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFields = [
-      "name",
-      "email",
-      "password",
-      "passwordConfirmation",
-    ];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+      return {
+        statusCode: 200,
+        body: {},
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValid) {
-      return badRequest(new InvalidParamError("email"));
-    }
-    return {
-      statusCode: 200,
-      body: {},
-    };
   }
 }
 
