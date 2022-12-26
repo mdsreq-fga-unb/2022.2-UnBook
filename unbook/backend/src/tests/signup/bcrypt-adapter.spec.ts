87cd7a4c279ba81d684804102e6527a5c7e7d7f3
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-26 18:03:53 -0300

    :test_tube:	test: garante que o BcryptAdapter retorna o password criptografado

--- unbook/backend/src/tests/signup/bcrypt-adapter.spec.ts
+++ unbook/backend/src/tests/signup/bcrypt-adapter.spec.ts
@@ -1,6 +1,14 @@
 import bcrypt from "bcrypt";
 import { BcryptAdapter } from "../../modules/signup/infra/criptography/BcryptAdapter";
 
+jest.mock("bcrypt", () => {
+  return {
+    async hash(): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
+    },
+  };
+});
+
 describe("Bcrypt Adapter", () => {
   test("Deve chamar o bcrypt com os valores corretos", async () => {
     const salt = 12;
@@ -8,5 +16,12 @@ describe("Bcrypt Adapter", () => {
     const hashSpy = jest.spyOn(bcrypt, "hash");
     await sut.encrypt("password");
     expect(hashSpy).toHaveBeenCalledWith("password", 12);
+  });
+
+  test("Deve retornar o password criptografado", async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const hashed_password = await sut.encrypt("password");
+    expect(hashed_password).toBe("hashed_password");
   });
 });
