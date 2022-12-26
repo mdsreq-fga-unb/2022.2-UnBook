6f0bec6a19657e89b9679c251815de1f6cf5ff2d
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-25 22:48:47 -0300

    :test_tube: test: garante que o SingUpController retorna 200 quando todas as informações inseridas são válidas

--- unbook/backend/src/tests/signup/singup.spec.ts
+++ unbook/backend/src/tests/signup/singup.spec.ts
@@ -218,4 +218,24 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
   });
+
+  test("Deve retornar 200 os dados forem enviados corretamente", () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: "valid_name",
+        email: "valid_email@mail.com",
+        password: "valid_password",
+        passwordConfirmation: "valid_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email@mail.com",
+      password: "valid_password",
+    });
+  });
 });
