b82795a772050eeb96b88db38396fe0aa909c5c1
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-26 14:36:15 -0300

    :test_tube: test: garante que o EmailValidator Adapter aceite apenas e-mails com domínio @aluno.unb.br

--- unbook/backend/src/tests/accounts/email-validator-adapter.spec.ts
+++ unbook/backend/src/tests/accounts/email-validator-adapter.spec.ts
@@ -23,8 +23,14 @@ describe("EmailValidator Adapter", () => {
 
   test("Deve retornar verdadeiro se a validação de email retornar verdadeiro", () => {
     const sut = makeSut();
-    const isValid = sut.isValid("valid_email@mail.com");
+    const isValid = sut.isValid("any_email@aluno.unb.br");
     expect(isValid).toBe(true);
+  });
+
+  test("Deve retornar falso se o email enviado não possuir o domínio @aluno.unb.br", () => {
+    const sut = makeSut();
+    const isValid = sut.isValid("any_email@mail.com");
+    expect(isValid).toBe(false);
   });
 
   test("Deve chamar o email validator com um email correto", () => {
