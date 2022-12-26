fd02d9dcd9ea5bd4af328e1c48ccb7e50fdfd524
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-26 10:32:37 -0300

    :recycle: refactor: cria makeSut para evitar a duplicação de códigos

--- unbook/backend/src/tests/accounts/email-validator-adapter.spec.ts
+++ unbook/backend/src/tests/accounts/email-validator-adapter.spec.ts
@@ -9,22 +9,26 @@ jest.mock("validator", () => {
   };
 });
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter();
+};
+
 describe("EmailValidator Adapter", () => {
   test("Deve retornar falso se a validação de email retornar falso", () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
   });
 
   test("Deve retornar verdadeiro se a validação de email retornar verdadeiro", () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isValid = sut.isValid("valid_email@mail.com");
     expect(isValid).toBe(true);
   });
 
   test("Deve chamar o email validator com um email correto", () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isEmailSpy = jest.spyOn(validator, "isEmail");
     sut.isValid("any_email@mail.com");
     expect(isEmailSpy).toHaveBeenCalledWith("any_email@mail.com");
