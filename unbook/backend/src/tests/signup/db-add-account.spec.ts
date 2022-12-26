ecb964e6b141162aba37325b8e06079b0021f132
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-26 15:52:07 -0300

    :recycle: refactor: faz a criação do makeSut para DbAccount e Encrypter

--- unbook/backend/src/tests/signup/db-add-account.spec.ts
+++ unbook/backend/src/tests/signup/db-add-account.spec.ts
@@ -1,15 +1,30 @@
+/* eslint-disable max-classes-per-file */
+import { IEncrypter } from "../../modules/signup/data/protocols/encrypter";
 import { DbAddAccount } from "../../modules/signup/data/repositories/DbAddAccount";
+
+interface ISutTypes {
+  sut: DbAddAccount;
+  encrypterStub: IEncrypter;
+}
+
+const makeSut = (): ISutTypes => {
+  class EncrypterStub {
+    encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
+    }
+  }
+
+  const encrypterStub = new EncrypterStub();
+  const sut = new DbAddAccount(encrypterStub);
+  return {
+    sut,
+    encrypterStub,
+  };
+};
 
 describe("DbAddAccount UseCase", () => {
   test("Deve chamar o Encrypter com o password correto", async () => {
-    class EncrypterStub {
-      encrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve("hashed_password"));
-      }
-    }
-
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbAddAccount(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
     const encrypterSpy = jest.spyOn(encrypterStub, "encrypt");
     const accountData = {
       name: "valid_name",
