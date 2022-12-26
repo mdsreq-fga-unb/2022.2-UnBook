347801769e34d81fed31d7c25bc9eaef7d1fc44d
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-26 10:25:31 -0300

    :test_tube: test: garante que o EmailValidator Adapter retorna falso quando é um email imválido e verdadeiro quando o email é válido

--- unbook/backend/src/tests/accounts/email-validator-adapter.spec.ts
+++ unbook/backend/src/tests/accounts/email-validator-adapter.spec.ts
@@ -1,9 +1,25 @@
+import validator from "validator";
 import { EmailValidatorAdapter } from "../../modules/accounts/utils/EmailValidatorAdapter";
+
+jest.mock("validator", () => {
+  return {
+    isEmail(): boolean {
+      return true;
+    },
+  };
+});
 
 describe("EmailValidator Adapter", () => {
   test("Deve retornar falso se a validação de email retornar falso", () => {
     const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
+  });
+
+  test("Deve retornar verdadeiro se a validação de email retornar verdadeiro", () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid("valid_email@mail.com");
+    expect(isValid).toBe(true);
   });
 });
