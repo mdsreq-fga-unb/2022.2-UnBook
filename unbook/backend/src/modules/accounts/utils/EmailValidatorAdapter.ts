26dbba816fba063152bfd5f1ff6f2dcd9481b63b
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-26 14:31:24 -0300

    :sparkles: feat: garante que o EmailValidator Adapter aceite apenas e-mails com domínio @aluno.unb.br

--- unbook/backend/src/modules/accounts/utils/EmailValidatorAdapter.ts
+++ unbook/backend/src/modules/accounts/utils/EmailValidatorAdapter.ts
@@ -3,7 +3,7 @@ import { IEmailValidator } from "../presentation/controllers/SignUpProtocols";
 
 class EmailValidatorAdapter implements IEmailValidator {
   isValid(email: string): boolean {
-    return validator.isEmail(email);
+    return validator.isEmail(email) && email.includes("@aluno.unb.br");
   }
 }
 
