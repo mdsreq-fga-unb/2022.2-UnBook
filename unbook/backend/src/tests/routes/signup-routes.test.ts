de2b84a1837884664ef4c0905e9f31520a2d074a
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-27 18:34:55 -0300

    :wrench: chore: conecta o banco de dados com os testes de integração

--- unbook/backend/src/tests/routes/signup-routes.test.ts
+++ unbook/backend/src/tests/routes/signup-routes.test.ts
@@ -1,7 +1,20 @@
 import request from "supertest";
+import { MongoHelper } from "../../infra/database/mongodb/helpers/mongo-helper";
 import { app } from "../../main/config/app";
 
 describe("SignUp Routes", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    MongoHelper.getCollection("accounts").deleteMany({});
+  });
+
   test("Garante que o retorno seja uma conta", async () => {
     await request(app)
       .post("/api/signup")
