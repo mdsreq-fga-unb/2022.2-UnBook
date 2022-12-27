a3c1fae990c4e5200d118b1dc790d69d4081dcc4
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-27 17:13:01 -0300

    :sparkles: feat: garante que o retorno das requisições sejam json, por padrão

--- unbook/backend/src/main/config/middlewares.ts
+++ unbook/backend/src/main/config/middlewares.ts
@@ -1,10 +1,12 @@
 import { Express } from "express";
 import { bodyParser } from "../middlewares/body-parser";
+import { contentType } from "../middlewares/content-type";
 import { cors } from "../middlewares/cors";
 
 const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser);
   app.use(cors);
+  app.use(contentType);
 };
 
 export { setupMiddlewares };
