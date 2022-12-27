dedf46e17cbc49c414fd232843428a5324734587
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-27 15:09:26 -0300

    :recycle: refactor: cria makeSut para o AccountModelRepository

--- unbook/backend/src/tests/signup/account-mongo-repository.spec.ts
+++ unbook/backend/src/tests/signup/account-mongo-repository.spec.ts
@@ -10,8 +10,12 @@ describe("Account Mongo Repository", () => {
     await MongoHelper.disconnect();
   });
 
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository();
+  };
+
   test("Deve retornar uma account quando tiver sucesso", async () => {
-    const sut = new AccountMongoRepository();
+    const sut = makeSut();
     const account = await sut.add({
       name: "any_name",
       email: "any_email@mail.com",
