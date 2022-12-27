d8907e96769acdc11c5e7fc14c551bc55cf501b8
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-27 16:48:24 -0300

    :sparkles: feat: configura o core

--- unbook/backend/src/main/config/middlewares.ts
+++ unbook/backend/src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { bodyParser } from "../middlewares/body-parser";
+import { cors } from "../middlewares/cors";
 
 const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };
 
 export { setupMiddlewares };
