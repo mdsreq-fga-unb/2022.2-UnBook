4f349a48b8052eccf633ed8bb7f5bdd2dd013896
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-27 21:13:25 -0300

    :recycle: refactor: adiciona o env no gitignore

--- unbook/backend/src/main/config/env.ts
+++
@@ -1,9 +0,0 @@
-const DB_USER = "unbook";
-const DB_PASSWORD = "XS30zlzxhc7kOMwQ";
-
-export default {
-  mongoUrl:
-    process.env.MONGO_URL ||
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@unbook.ypguopm.mongodb.net/test`,
-  port: process.env.PORT || 3000,
-};
