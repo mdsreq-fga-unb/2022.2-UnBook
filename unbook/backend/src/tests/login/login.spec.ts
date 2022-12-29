1086d2d3475de5f728ebbe6d864ac68c5a59ae29
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-28 23:49:23 -0300

    :recycle: refactor: remove código duplicado

--- unbook/backend/src/tests/login/login.spec.ts
+++ unbook/backend/src/tests/login/login.spec.ts
@@ -5,7 +5,10 @@ import {
   MissingParamError,
 } from "../../presentation/errors";
 import { badRequest } from "../../presentation/helpers/http-helper";
-import { IEmailValidator } from "../../presentation/protocols/signup-protocols";
+import {
+  IEmailValidator,
+  IHttpRequest,
+} from "../../presentation/protocols/signup-protocols";
 
 interface ISubTypes {
   sut: LoginController;
@@ -30,25 +33,28 @@ const makeSut = (): ISubTypes => {
   };
 };
 
+const makeFakeRequest = (): IHttpRequest => {
+  return {
+    body: {
+      email: "any_email@mail.com",
+      password: "any_password",
+    },
+  };
+};
+
 describe("SignUp Controller", () => {
   test("Deve retornar 400 se o email não for fornecido", async () => {
     const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        password: "any_password",
-      },
-    };
+    const httpRequest = makeFakeRequest();
+    delete httpRequest.body.email;
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
   });
 
   test("Deve retornar 400 se a senha não for fornecida", async () => {
     const { sut } = makeSut();
-    const httpRequest = {
-      body: {
-        email: "any_email@mail.com",
-      },
-    };
+    const httpRequest = makeFakeRequest();
+    delete httpRequest.body.password;
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
   });
@@ -56,26 +62,14 @@ describe("SignUp Controller", () => {
   test("Deve retornar 400 se o email fornecido for inválido", async () => {
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false);
-    const httpRequest = {
-      body: {
-        email: "any_email@mail.com",
-        password: "any_password",
-      },
-    };
-    const httpResponse = await sut.handle(httpRequest);
+    const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(badRequest(new InvalidParamError("email")));
   });
 
   test("Deve chamar o EmailValidator com o email correto", async () => {
     const { sut, emailValidatorStub } = makeSut();
     const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
-    const httpRequest = {
-      body: {
-        email: "any_email@mail.com",
-        password: "any_password",
-      },
-    };
-    await sut.handle(httpRequest);
+    await sut.handle(makeFakeRequest());
     expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
 });
