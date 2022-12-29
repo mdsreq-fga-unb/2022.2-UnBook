834af3ad0f510ed351971e8cb2efc2fc740a28f0
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-29 14:56:59 -0300

    :recycle: refactor: move a criação do sut para um factory

--- unbook/backend/src/tests/factories/validation-composite.spec.ts
+++ unbook/backend/src/tests/factories/validation-composite.spec.ts
@@ -1,17 +1,39 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { MissingParamError } from "../../presentation/errors";
 import { ValidationComposite } from "../../presentation/helpers/validators/ValidationComposite";
+import { IValidation } from "../../presentation/protocols/signup-protocols";
+
+interface ISubTypes {
+  sut: ValidationComposite;
+  validationStub: IValidation;
+}
+
+const makeValidation = (): IValidation => {
+  class ValidationStub implements IValidation {
+    validate(input: any): Error | undefined {
+      return undefined;
+    }
+  }
+  return new ValidationStub();
+};
+
+const makeSut = (): ISubTypes => {
+  const validationStub = makeValidation();
+  const sut = new ValidationComposite([validationStub]);
+  return {
+    sut,
+    validationStub,
+  };
+};
 
 describe("Validation Composite", () => {
   test("Deve retornar um erro se alguma validação falhar", () => {
-    class ValidationStub {
-      validate(input: any): Error {
-        return new MissingParamError("field");
-      }
-    }
-    const validationStub = new ValidationStub();
-    const sut = new ValidationComposite([validationStub]);
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, "validate")
+      .mockReturnValueOnce(new MissingParamError("field"));
     const error = sut.validate({ field: "any_value" });
     expect(error).toEqual(new MissingParamError("field"));
   });
