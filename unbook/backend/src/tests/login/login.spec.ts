93b33185c3b2e2c03506f2b4dafa0e68eeeaedf6
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-28 23:29:03 -0300

    :sparkles: feat: cria makeSut para signController

--- unbook/backend/src/tests/login/login.spec.ts
+++ unbook/backend/src/tests/login/login.spec.ts
@@ -2,9 +2,20 @@ import { LoginController } from "../../presentation/controllers/LogInController"
 import { MissingParamError } from "../../presentation/errors";
 import { badRequest } from "../../presentation/helpers/http-helper";
 
+interface ISubTypes {
+  sut: LoginController;
+}
+
+const makeSut = (): ISubTypes => {
+  const sut = new LoginController();
+  return {
+    sut,
+  };
+};
+
 describe("SignUp Controller", () => {
   test("Deve retornar 400 se o email não for fornecido", async () => {
-    const sut = new LoginController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: "any_password",
@@ -15,7 +26,7 @@ describe("SignUp Controller", () => {
   });
 
   test("Deve retornar 400 se a senha não for fornecida", async () => {
-    const sut = new LoginController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: "any_email@mail.com",
