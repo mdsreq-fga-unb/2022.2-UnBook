8cc32de54aad740d5695649eb1067e48ae36ec5e
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-28 23:56:22 -0300

    :sparkles: feat: garante que o LogInController retorna 500 se o EmailValidator lançar um erro

--- unbook/backend/src/presentation/controllers/LogInController.ts
+++ unbook/backend/src/presentation/controllers/LogInController.ts
@@ -1,5 +1,5 @@
-import { InvalidParamError, MissingParamError } from "../errors";
-import { badRequest } from "../helpers/http-helper";
+import { InvalidParamError, MissingParamError, ServerError } from "../errors";
+import { badRequest, serverError } from "../helpers/http-helper";
 import { IController, IHttpRequest, IHttpResponse } from "../protocols";
 import { IEmailValidator } from "../protocols/signup-protocols";
 
@@ -8,26 +8,30 @@ class LoginController implements IController {
     this.emailValidator = emailValidator;
   }
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const { email, password } = httpRequest.body;
-    if (!email) {
+    try {
+      const { email, password } = httpRequest.body;
+      if (!email) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new MissingParamError("email")))
+        );
+      }
+      if (!password) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new MissingParamError("password")))
+        );
+      }
+      const isValid = this.emailValidator.isValid(email);
+      if (!isValid) {
+        return new Promise((resolve) =>
+          resolve(badRequest(new InvalidParamError("email")))
+        );
+      }
       return new Promise((resolve) =>
         resolve(badRequest(new MissingParamError("email")))
       );
+    } catch (error) {
+      return serverError(error as Error);
     }
-    if (!password) {
-      return new Promise((resolve) =>
-        resolve(badRequest(new MissingParamError("password")))
-      );
-    }
-    const isValid = this.emailValidator.isValid(email);
-    if (!isValid) {
-      return new Promise((resolve) =>
-        resolve(badRequest(new InvalidParamError("email")))
-      );
-    }
-    return new Promise((resolve) =>
-      resolve(badRequest(new MissingParamError("email")))
-    );
   }
 }
 
