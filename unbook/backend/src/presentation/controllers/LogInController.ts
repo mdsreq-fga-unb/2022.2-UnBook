f1793ed0cd613c2220c73b61d0dd6852bfdb32a7
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-28 23:37:03 -0300

    :sparkles: feat: garante que o LoginController chama o EmailValidator com o email correto

--- unbook/backend/src/presentation/controllers/LogInController.ts
+++ unbook/backend/src/presentation/controllers/LogInController.ts
@@ -1,8 +1,12 @@
 import { MissingParamError } from "../errors";
 import { badRequest } from "../helpers/http-helper";
 import { IController, IHttpRequest, IHttpResponse } from "../protocols";
+import { IEmailValidator } from "../protocols/signup-protocols";
 
 class LoginController implements IController {
+  constructor(private readonly emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator;
+  }
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     if (!httpRequest.body.email) {
       return new Promise((resolve) =>
@@ -14,6 +18,7 @@ class LoginController implements IController {
         resolve(badRequest(new MissingParamError("password")))
       );
     }
+    this.emailValidator.isValid(httpRequest.body.email);
     return new Promise((resolve) =>
       resolve(badRequest(new MissingParamError("email")))
     );
