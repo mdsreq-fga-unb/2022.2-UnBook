856325a05743c77fe2ccf500969547328e2c7c17
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-29 14:33:43 -0300

    :recycle: refactor: move a criação do sut para um factory

--- unbook/backend/src/tests/factories/required-field-validation.spec.ts
+++ unbook/backend/src/tests/factories/required-field-validation.spec.ts
@@ -1,15 +1,19 @@
 import { MissingParamError } from "../../presentation/errors";
 import { RequiredFieldValidation } from "../../presentation/helpers/validators/RequiredFieldValidation";
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation("any_field");
+};
+
 describe("Required Field Validation", () => {
   test("Deve retornar um MissingParamError se a validação falhar", () => {
-    const sut = new RequiredFieldValidation("any_field");
+    const sut = makeSut();
     const error = sut.validate({ name: "any_parameter" });
     expect(error).toEqual(new MissingParamError("any_field"));
   });
 
   test("Não deve retornar nada se a validação tiver sucesso", () => {
-    const sut = new RequiredFieldValidation("any_field");
+    const sut = makeSut();
     const error = sut.validate({ any_field: "any_parameter" });
     expect(error).toBeFalsy();
   });
