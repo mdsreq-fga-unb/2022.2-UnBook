ef3821ab945000775006f7037a1534cd5cd78417
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-30 00:55:41 -0300

    :sparkles: feat: garante que o AccountMongoRepository retorna uma conta se o loadByEmail tiver sucesso e retorna null se falhar

--- unbook/backend/src/database/protocols/database/ILoadAccountByEmailRepository.ts
+++ unbook/backend/src/database/protocols/database/ILoadAccountByEmailRepository.ts
@@ -1,7 +1,7 @@
 import { IAccountModel } from "../../../presentation/protocols/signup-protocols";
 
 interface ILoadAccountByEmailRepository {
-  loadByEmail(email: string): Promise<IAccountModel | null>;
+  loadByEmail(email: string): Promise<IAccountModel>;
 }
 
 export { ILoadAccountByEmailRepository };
--- unbook/backend/src/infra/database/mongodb/repositories/AccountMongoRepository.ts
+++ unbook/backend/src/infra/database/mongodb/repositories/AccountMongoRepository.ts
@@ -1,19 +1,22 @@
 /* eslint-disable no-underscore-dangle */
 import { IAddAccountRepository } from "../../../../database/protocols/database/data-sign-up-protocols";
+import { ILoadAccountByEmailRepository } from "../../../../database/protocols/database/ILoadAccountByEmailRepository";
 import {
   IAccountModel,
   IAddAccountModel,
 } from "../../../../presentation/protocols/signup-protocols";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-class AccountMongoRepository implements IAddAccountRepository {
+class AccountMongoRepository
+  implements IAddAccountRepository, ILoadAccountByEmailRepository
+{
   async add(accountData: IAddAccountModel): Promise<IAccountModel> {
     const accountCollection = MongoHelper.getCollection("accounts");
     const result = await accountCollection.insertOne(accountData);
     const id = result.insertedId;
     const account = await accountCollection.findOne({ _id: id });
 
-    if (account !== null) {
+    if (account) {
       return {
         id: account._id.toString(),
         name: account.name,
@@ -22,12 +25,21 @@ class AccountMongoRepository implements IAddAccountRepository {
       };
     }
 
-    return {
-      id: "any_id",
-      name: "any_name",
-      email: "any_email@mail.com",
-      password: "any_password",
-    };
+    return null;
+  }
+
+  async loadByEmail(email: string): Promise<IAccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    const account = await accountCollection.findOne({ email });
+    if (account) {
+      return {
+        id: account._id.toString(),
+        name: account.name,
+        email: account.email,
+        password: account.password,
+      };
+    }
+    return null;
   }
 }
 
