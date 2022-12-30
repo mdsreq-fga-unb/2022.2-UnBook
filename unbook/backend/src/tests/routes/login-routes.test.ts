5baf130120d8f7d8f0d139ae606c00c569ee8e2c
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-30 12:59:53 -0300

    :recycle: refactor: renomeia signup routes para login routes

--- unbook/backend/src/tests/routes/login-routes.test.ts
+++ unbook/backend/src/tests/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import { MongoHelper } from "../../infra/database/mongodb/helpers/mongo-helper";
 import { app } from "../../main/config/app";
 
-describe("SignUp Routes", () => {
+describe("Login Routes", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string);
   });
@@ -15,15 +15,17 @@ describe("SignUp Routes", () => {
     MongoHelper.getCollection("accounts").deleteMany({});
   });
 
-  test("Garante que o retorno seja uma conta", async () => {
-    await request(app)
-      .post("/api/signup")
-      .send({
-        name: "any_name",
-        email: "any_email@aluno.unb.br",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      })
-      .expect(200);
+  describe("POST/ signup", () => {
+    test("Deve retornar 200 quando o signup tem sucesso", async () => {
+      await request(app)
+        .post("/api/signup")
+        .send({
+          name: "any_name",
+          email: "any_email@aluno.unb.br",
+          password: "any_password",
+          passwordConfirmation: "any_password",
+        })
+        .expect(200);
+    });
   });
 });
