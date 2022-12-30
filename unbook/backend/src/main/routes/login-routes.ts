ad92b9aa62d93e5e12fbda4490995e6026ab0b1f
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-30 13:48:11 -0300

    :sparkles: feat: adiciona a rota de login

--- unbook/backend/src/main/routes/login-routes.ts
+++ unbook/backend/src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express/express-route-adapter";
+import { makeLogInController } from "../factories/login/login-factory";
 import { makeSignUpController } from "../factories/signup/signup-factory";
 
 export default (router: Router): void => {
   router.post("/signup", adaptRoute(makeSignUpController()));
+  router.post("/login", adaptRoute(makeLogInController()));
 };
