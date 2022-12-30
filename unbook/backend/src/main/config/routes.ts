c9e15a0bfae6481002d74923a4ba1743d93c1e43
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-30 15:20:59 -0300

    :recycle: refactor: faz alteração na forma que as rotas são chamadas, exclui fast -glob e adiciona filesystem

--- unbook/backend/src/main/config/routes.ts
+++ unbook/backend/src/main/config/routes.ts
@@ -1,11 +1,13 @@
 import { Express, Router } from "express";
-import fg from "fast-glob";
+import { readdirSync } from "fs";
 
 const setupRoutes = (app: Express): void => {
   const router = Router();
   app.use("/api", router);
-  fg.sync(["**/src/main/routes/**routes.ts"]).map(async (file) => {
-    (await import(`../../../${file}`)).default(router);
+  readdirSync(`${__dirname}/../routes`).map(async (file) => {
+    if (!file.includes(".test.")) {
+      (await import(`../routes/${file}`)).default(router);
+    }
   });
 };
 
