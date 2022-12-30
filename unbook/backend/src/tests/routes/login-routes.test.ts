d90e48ca8148372ba8a46fa670925b0f0907bad6
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-30 13:48:54 -0300

    :test_tube:	test: garante que a rota de login retorna 200 quando o login tem sucesso

--- unbook/backend/src/tests/routes/login-routes.test.ts
+++ unbook/backend/src/tests/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
+import { hash } from "bcrypt";
+import { Collection } from "mongodb";
 import request from "supertest";
 import { MongoHelper } from "../../infra/database/mongodb/helpers/mongo-helper";
 import { app } from "../../main/config/app";
+
+let accountCollection: Collection;
 
 describe("Login Routes", () => {
   beforeAll(async () => {
@@ -12,11 +16,12 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    MongoHelper.getCollection("accounts").deleteMany({});
+    accountCollection = MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
   });
 
   describe("POST/ signup", () => {
-    test("Deve retornar 200 quando o signup tem sucesso", async () => {
+    test("Deve retornar 200 quando o signup tiver sucesso", async () => {
       await request(app)
         .post("/api/signup")
         .send({
@@ -24,6 +29,24 @@ describe("Login Routes", () => {
           email: "any_email@aluno.unb.br",
           password: "any_password",
           passwordConfirmation: "any_password",
+        })
+        .expect(200);
+    });
+  });
+
+  describe("POST/ login", () => {
+    test("Deve retornar 200 quando o login tiver sucesso", async () => {
+      const password = await hash("any_password", 12);
+      await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@aluno.unb.br",
+        password,
+      });
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "any_email@aluno.unb.br",
+          password: "any_password",
         })
         .expect(200);
     });
