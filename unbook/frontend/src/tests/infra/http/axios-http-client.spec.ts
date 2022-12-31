528f9bd708a533aecc5827546808b8b1468dd247
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-31 01:58:49 -0300

    :test_tube: test: garante que o AxiosHttpClient retorna os valores corretos

--- unbook/frontend/src/tests/infra/http/axios-http-client.spec.ts
+++ unbook/frontend/src/tests/infra/http/axios-http-client.spec.ts
@@ -5,6 +5,11 @@ import { HttpPostParams } from "../../../database/protocols/http";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+const mockedAxiosResult = {
+	data: faker.helpers.objectValue({}),
+	status: faker.internet.httpStatusCode(),
+};
+mockedAxios.post.mockResolvedValue(mockedAxiosResult);
 
 const makeSut = (): AxiosHttpClient => {
 	return new AxiosHttpClient();
@@ -21,5 +26,14 @@ describe("AxiosHttpClient", () => {
 		const sut = makeSut();
 		await sut.post(request);
 		expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+	});
+
+	test("Deve retornar o statusCode e body corretos", async () => {
+		const sut = makeSut();
+		const httpRespose = await sut.post(mockPostRequest());
+		expect(httpRespose).toEqual({
+			statusCode: mockedAxiosResult.status,
+			body: mockedAxiosResult.data,
+		});
 	});
 });
