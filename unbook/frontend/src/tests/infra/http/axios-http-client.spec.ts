f3ba9dbb1c2cf50e0d895753b84932a0db6fbf5f
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-31 01:42:16 -0300

    :recycle: refactor: move a criação do request para um factory method

--- unbook/frontend/src/tests/infra/http/axios-http-client.spec.ts
+++ unbook/frontend/src/tests/infra/http/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import { AxiosHttpClient } from "../../../infra/http/AxiosHttpClient";
 import axios from "axios";
 import { faker } from "@faker-js/faker";
+import { HttpPostParams } from "../../../database/protocols/http";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -9,11 +10,16 @@ const makeSut = (): AxiosHttpClient => {
 	return new AxiosHttpClient();
 };
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+	url: faker.internet.url(),
+	body: faker.helpers.objectValue({}),
+});
+
 describe("AxiosHttpClient", () => {
 	test("Deve chamar o axios com a url e verbo corretos", async () => {
-		const url = faker.internet.url();
+		const request = mockPostRequest();
 		const sut = makeSut();
-		await sut.post({ url });
-		expect(mockedAxios.post).toHaveBeenCalledWith(url);
+		await sut.post(request);
+		expect(mockedAxios.post).toHaveBeenCalledWith(request.url);
 	});
 });
