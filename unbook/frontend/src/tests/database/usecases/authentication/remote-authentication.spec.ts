ebff7e9963e036bce5094c0b3bf7bd506ebb6b30
Maciel Júnior <macielfcjunior@icloud.com>
2022-12-30 21:51:35 -0300

    :recycle: refactor: move a criação do sut para um factory method

--- unbook/frontend/src/tests/database/usecases/authentication/remote-authentication.spec.ts
+++ unbook/frontend/src/tests/database/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,25 @@
 import { RemoteAuthentication } from "../../../../database/repositories/RemoteAuthentication";
 import { HttpPostClientSpy } from "../../../mocks/mock-http-client";
 
+interface ISubTypes {
+	sut: RemoteAuthentication;
+	httpPostClientSpy: HttpPostClientSpy;
+}
+
+const makeSut = (url = "any_url"): ISubTypes => {
+	const httpPostClientSpy = new HttpPostClientSpy();
+	const sut = new RemoteAuthentication(url, httpPostClientSpy);
+
+	return {
+		sut,
+		httpPostClientSpy,
+	};
+};
+
 describe("RemoteAuthentication", () => {
 	test("Deve chamar HttpPostClient com a URL correta", async () => {
-		const url = "any_url";
-		const httpPostClientSpy = new HttpPostClientSpy();
-		const sut = new RemoteAuthentication(url, httpPostClientSpy);
+		const url = "other_url";
+		const { sut, httpPostClientSpy } = makeSut(url);
 		await sut.auth();
 		expect(httpPostClientSpy.url).toBe(url);
 	});
