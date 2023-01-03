b695ed7bf960f6661d00aebd0ebb8ab59d372e1d
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-03 14:47:13 -0300

    :recycle: refactor: move a criação do sut do login para um factory method

--- unbook/frontend/src/tests/presentation/pages/login/login.spec.tsx
+++ unbook/frontend/src/tests/presentation/pages/login/login.spec.tsx
@@ -1,29 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { RenderResult, render } from "@testing-library/react";
 import { Login } from "../../../../presentation/pages";
+
+interface ISutTypes {
+	sut: RenderResult;
+}
+
+const makeSut = (): ISutTypes => {
+	const sut = render(<Login />);
+	return {
+		sut,
+	};
+};
 
 describe("Login Component", () => {
 	test("Não deve renderizar o spinner e o erro no início", () => {
-		const { getByTestId } = render(<Login />);
-		const errorWrapper = getByTestId("errorWrapper");
+		const { sut } = makeSut();
+		const errorWrapper = sut.getByTestId("errorWrapper");
 		expect(errorWrapper.childElementCount).toBe(0);
 	});
 
 	test("Deve garantir que o botão de entrar esteja desabilitado se a senha e email não estiverem preenchidos", () => {
-		const { getByTestId } = render(<Login />);
-		const submitButton = getByTestId("submit") as HTMLButtonElement;
+		const { sut } = makeSut();
+		const submitButton = sut.getByTestId("submit") as HTMLButtonElement;
 		expect(submitButton.disabled).toBe(true);
 	});
 
 	test("Deve garantir que o título do input email deva ser campo obrigatório", () => {
-		const { getByTestId } = render(<Login />);
-		const emailStatus = getByTestId("email-status");
+		const { sut } = makeSut();
+		const emailStatus = sut.getByTestId("email-status");
 		expect(emailStatus.title).toBe("Campo obrigatório");
 	});
 
 	test("Deve garantir que o título do input deva password ser campo obrigatório", () => {
-		const { getByTestId } = render(<Login />);
-		const passwordStatus = getByTestId("password-status");
+		const { sut } = makeSut();
+		const passwordStatus = sut.getByTestId("password-status");
 		expect(passwordStatus.title).toBe("Campo obrigatório");
 	});
 });
