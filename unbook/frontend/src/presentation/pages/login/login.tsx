8c7a83aac4401458a6811bbe5467daf971eded68
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-03 20:49:19 -0300

    :sparkles:	feat: garante que o Login apresenta um erro se o password for inválido

--- unbook/frontend/src/presentation/pages/login/login.tsx
+++ unbook/frontend/src/presentation/pages/login/login.tsx
@@ -23,18 +23,16 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
 		email: "",
 		password: "",
 		emailError: "",
-		passwordError: "Campo obrigatório",
+		passwordError: "",
 		mainError: "",
 	});
 	useEffect(() => {
 		setState({
 			...state,
 			emailError: validation.validate("email", state.email),
+			passwordError: validation.validate("password", state.password),
 		});
-	}, [state.email]);
-	useEffect(() => {
-		validation.validate("password", state.password);
-	}, [state.password]);
+	}, [state.email, state.password]);
 
 	return (
 		<div className={styles.login}>
