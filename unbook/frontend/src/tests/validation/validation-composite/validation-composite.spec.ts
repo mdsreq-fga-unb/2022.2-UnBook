ea5fb281337f75200f7cdf028c39a080bfc0a6a1
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-04 22:00:37 -0300

    :test_tube: test: garante que o Validation composite retorna o primeiro erro caso haja mais de um

--- unbook/frontend/src/tests/validation/validation-composite/validation-composite.spec.ts
+++ unbook/frontend/src/tests/validation/validation-composite/validation-composite.spec.ts
@@ -4,13 +4,14 @@ import { FieldValidationSpy } from "../mocks/Mock-FieldValidationSpy";
 describe("ValidationComposite", () => {
 	test("Deve retornar erro se alguma validação falhar", () => {
 		const fieldValidationSpy_1 = new FieldValidationSpy("any_field");
+		fieldValidationSpy_1.error = new Error("first_error_message");
 		const fieldValidationSpy_2 = new FieldValidationSpy("any_field");
-		fieldValidationSpy_2.error = new Error("any_error_message");
+		fieldValidationSpy_2.error = new Error("second_error_message");
 		const sut = new ValidationComposite([
 			fieldValidationSpy_1,
 			fieldValidationSpy_2,
 		]);
 		const error = sut.validate("any_field", "any_value");
-		expect(error).toBe("any_error_message");
+		expect(error).toBe("first_error_message");
 	});
 });
