1996968ec8555c0a1a8e837af5fef2f41979706c
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-04 21:06:29 -0300

    :test_tube: test: garante que o EmailValidatio retorna falso se o email for inválido

--- unbook/frontend/src/tests/validation/email/email-validation.spec.ts
+++ unbook/frontend/src/tests/validation/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidParamError } from "../../../validation/errors/invalid-param-error";
 import { EmailValidation } from "../../../validation/validators/email/email-validation";
+import { faker } from "@faker-js/faker";
 
 describe("EmailValidation", () => {
 	test("Deve retornar erro se o email for inválido", () => {
 		const sut = new EmailValidation("email");
 		const error = sut.validate(sut.field);
 		expect(error).toEqual(new InvalidParamError("email"));
+	});
+
+	test("Deve retornar falso se o email for inválido", () => {
+		const sut = new EmailValidation("email");
+		const error = sut.validate(faker.internet.email());
+		expect(error).toBeFalsy();
 	});
 });
