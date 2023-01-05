1a02cc6e43a86d256c96a1a959b9c69690b2dbd9
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-04 21:12:35 -0300

    :recycle:	refactor: move o sut para um factory method

--- unbook/frontend/src/tests/validation/email/email-validation.spec.ts
+++ unbook/frontend/src/tests/validation/email/email-validation.spec.ts
@@ -2,15 +2,19 @@ import { InvalidParamError } from "../../../validation/errors/invalid-param-erro
 import { EmailValidation } from "../../../validation/validators/email/email-validation";
 import { faker } from "@faker-js/faker";
 
+const makeSut = (field = faker.database.column()): EmailValidation => {
+	return new EmailValidation(field);
+};
+
 describe("EmailValidation", () => {
 	test("Deve retornar erro se o email for inválido", () => {
-		const sut = new EmailValidation("email");
+		const sut = makeSut("email");
 		const error = sut.validate(sut.field);
 		expect(error).toEqual(new InvalidParamError("email"));
 	});
 
 	test("Deve retornar falso se o email for inválido", () => {
-		const sut = new EmailValidation("email");
+		const sut = makeSut();
 		const error = sut.validate(faker.internet.email());
 		expect(error).toBeFalsy();
 	});
