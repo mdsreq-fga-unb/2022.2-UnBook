e6aa22e8ec83fa2d0497f8fb317af35c96ee6645
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-04 21:31:23 -0300

    :test_tube: test: garante que o MinLengthValidation retorna falso se o valor for inválido

--- unbook/frontend/src/tests/validation/min-lenth/min-lenth-validadtion.spec.ts
+++ unbook/frontend/src/tests/validation/min-lenth/min-lenth-validadtion.spec.ts
@@ -1,10 +1,24 @@
+import { faker } from "@faker-js/faker";
 import { InvalidParamError } from "../../../validation/errors/invalid-param-error";
 import { MinLengthValidation } from "../../../validation/validators/min-lenth/min-lenth-validadtion";
 
+const makeSut = (
+	field = faker.database.column(),
+	minLength: number
+): MinLengthValidation => {
+	return new MinLengthValidation(field, minLength);
+};
+
 describe("MinLengthValidation", () => {
 	test("Deve retornar erro se o valor for inválido", () => {
-		const sut = new MinLengthValidation("field", 5);
+		const sut = makeSut("field", 5);
 		const error = sut.validate("123");
 		expect(error).toEqual(new InvalidParamError("field"));
+	});
+
+	test("Deve retornar falso se o valor for válido", () => {
+		const sut = makeSut("field", 5);
+		const error = sut.validate("123456");
+		expect(error).toBeFalsy();
 	});
 });
