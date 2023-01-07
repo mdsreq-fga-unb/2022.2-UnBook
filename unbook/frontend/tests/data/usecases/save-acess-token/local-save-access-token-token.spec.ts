622912e740f1c6fb5d3d94e167efef920fe24a9d
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-07 15:53:32 -0300

    :recycle: refactor: move a criação do sut para um factory method

--- unbook/frontend/tests/data/usecases/save-acess-token/local-save-access-token-token.spec.ts
+++ unbook/frontend/tests/data/usecases/save-acess-token/local-save-access-token-token.spec.ts
@@ -2,10 +2,23 @@ import { LocalSaveAccessToken } from "../../../../src/data/repositories/LocalSav
 import { faker } from "@faker-js/faker";
 import { SetStorageSpy } from "../../mocks/mock-storage";
 
+type SutTypes = {
+	sut: LocalSaveAccessToken;
+	setStorageSpy: SetStorageSpy;
+};
+
+const makeSut = (): SutTypes => {
+	const setStorageSpy = new SetStorageSpy();
+	const sut = new LocalSaveAccessToken(setStorageSpy);
+	return {
+		sut,
+		setStorageSpy,
+	};
+};
+
 describe("LocalSaveAccessToken", () => {
 	test("Deve chamar SetStorage com os valores corretos", async () => {
-		const setStorageSpy = new SetStorageSpy();
-		const sut = new LocalSaveAccessToken(setStorageSpy);
+		const { sut, setStorageSpy } = makeSut();
 		const accessToken = faker.datatype.uuid();
 		await sut.save(accessToken);
 		expect(setStorageSpy.key).toBe("accessToken");
