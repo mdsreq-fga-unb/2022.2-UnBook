4da9c53785977755d65f7e9c4f576514d960ed6f
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-07 16:03:59 -0300

    :test_tube: test: garante que LocalSaveAccessToken retorna um erro se o SetStorage lançar um erro

--- unbook/frontend/tests/data/usecases/save-acess-token/local-save-access-token-token.spec.ts
+++ unbook/frontend/tests/data/usecases/save-acess-token/local-save-access-token-token.spec.ts
@@ -1,6 +1,7 @@
 import { LocalSaveAccessToken } from "../../../../src/data/repositories/LocalSaveAccessToken";
 import { faker } from "@faker-js/faker";
 import { SetStorageMock } from "../../mocks/mock-storage";
+import { vi } from "vitest";
 
 type SutTypes = {
 	sut: LocalSaveAccessToken;
@@ -27,9 +28,10 @@ describe("LocalSaveAccessToken", () => {
 
 	test("Deve chamar SetStorage com os valores corretos", async () => {
 		const { sut, setStorageMock } = makeSut();
-		const accessToken = faker.datatype.uuid();
-		await sut.save(accessToken);
-		expect(setStorageMock.key).toBe("accessToken");
-		expect(setStorageMock.value).toBe(accessToken);
+		vi.spyOn(setStorageMock, "set").mockReturnValueOnce(
+			Promise.reject(new Error())
+		);
+		const promise = sut.save(faker.datatype.uuid());
+		await expect(promise).rejects.toThrow(new Error());
 	});
 });
