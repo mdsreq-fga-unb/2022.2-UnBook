7e5745e88c57bd1cfb4ffea83b2df1b6fa3f19e1
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-06 23:25:03 -0300

    :test_tube:	test: garante que o SignUpController retorna 500 se o Authentications lançar um erro

--- unbook/backend/src/tests/signup/singup.spec.ts
+++ unbook/backend/src/tests/signup/singup.spec.ts
@@ -154,4 +154,13 @@ describe("SignUp Controller", () => {
       password: "any_password",
     });
   });
+
+  test("Deve retornar 500 se o Authentication retornar um erro", async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest.spyOn(authenticationStub, "auth").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
 });
