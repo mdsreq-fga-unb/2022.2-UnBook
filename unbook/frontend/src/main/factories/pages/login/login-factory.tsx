1a5f3b430a09e2481ce7fe9d289652ad0bc7b344
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-06 22:54:22 -0300

    :recycle:	refactor: move a url do login para um factory

--- unbook/frontend/src/main/factories/pages/login/login-factory.tsx
+++ unbook/frontend/src/main/factories/pages/login/login-factory.tsx
@@ -4,9 +4,10 @@ import { RemoteAuthentication } from "../../../../database/repositories/RemoteAu
 import { AxiosHttpClient } from "../../../../infra/http/AxiosHttpClient";
 import { ValidationComposite } from "../../../../validation/validators/validation-composite/ValidationComposite";
 import { ValidationBuilder } from "../../../../validation/validators/builder/ValidationBuilder";
+import { makeUrl } from "./makeUrl-factory";
 
 const makeLogin = (): JSX.Element => {
-	const url = "http://localhost:3000/api/login";
+	const url = makeUrl("login");
 	const axiosHttpClient = new AxiosHttpClient();
 	const remoteAuthentication = new RemoteAuthentication(url, axiosHttpClient);
 	const validationComposite = ValidationComposite.build([
