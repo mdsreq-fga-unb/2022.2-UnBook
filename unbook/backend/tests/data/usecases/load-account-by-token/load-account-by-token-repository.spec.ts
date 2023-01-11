b5157e5f437acce7fc899e35c9ff5810e33a28c1
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-11 02:35:18 -0300

    refactor: move a criação do sut para um factory method
    Co-authored-by: AnaBeatrizMassuh <aaamassuh@gmail.com>
    Co-authored-by: GabriellCabrall <gabrielbielcabral@gmail.com>

--- unbook/backend/tests/data/usecases/load-account-by-token/load-account-by-token-repository.spec.ts
+++ unbook/backend/tests/data/usecases/load-account-by-token/load-account-by-token-repository.spec.ts
@@ -1,16 +1,32 @@
 import { IDecrypter } from "../../../../src/data/protocols/criptography/IDecrypter";
 import { LoadAccountByTokenRepository } from "../../../../src/data/repositories/LoadAccountByTokenRepository";
 
+interface ISubTypes {
+  sut: LoadAccountByTokenRepository;
+  decrypterStub: IDecrypter;
+}
+
+const makeDecrypter = (): IDecrypter => {
+  class DecrypterStub implements IDecrypter {
+    async decrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("any_value"));
+    }
+  }
+  return new DecrypterStub();
+};
+const makeSut = (): ISubTypes => {
+  const decrypterStub = makeDecrypter();
+  const sut = new LoadAccountByTokenRepository(decrypterStub);
+  return {
+    sut,
+    decrypterStub,
+  };
+};
+
 describe("LoadAccountByTokenRepository", () => {
   test("Deve chamar o decrypter com os valores corretos", async () => {
-    class DecrypterStub implements IDecrypter {
-      async decrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve("any_value"));
-      }
-    }
-    const decrypterStub = new DecrypterStub();
+    const { sut, decrypterStub } = makeSut();
     const decryptSpy = jest.spyOn(decrypterStub, "decrypt");
-    const sut = new LoadAccountByTokenRepository(decrypterStub);
     await sut.load("any_token");
     expect(decryptSpy).toHaveBeenCalledWith("any_token");
   });
