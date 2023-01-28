98c2f8d011cc3f97a92f6d9dfe6a74a232d4d694
AnaBeatrizMassuh <aaamassuh@gmail.com>
2023-01-28 16:42:37 -0300

    Trocando navbar

--- UnBook/client/components/Nav.js
+++ UnBook/client/components/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav className={"nav d-flex justify-content-end primary bg-primary"}>
       <Link href="/" className="nav-link text-light">Página Inicial</Link>
-      <Link href="/login" className="nav-link active text-light">Entrar</Link>
+      <Link href="/login" className="nav-link active text-light">Login</Link>
       <Link href="/register" className="nav-link active text-light">Cadastrar</Link>
     </nav>
 
