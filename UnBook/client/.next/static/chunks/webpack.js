f0a6e7b5bcb9aad2e05f89ee74bacb3e51cf3022
gabriel cabral <gabrielbielcabral@gmail.com>
2023-01-28 09:23:02 -0300

    Recebe informações da página de registro

--- UnBook/client/.next/static/chunks/webpack.js
+++ UnBook/client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8839a201c60eab25"; }
+/******/ 		__webpack_require__.h = function() { return "bb6e0d29f8ca27cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
