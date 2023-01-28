43b7ab21d1618cb2a0367e305b1fafddec3e5fa8
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-28 18:56:49 -0300

    chore: adiciona a pasta next no gitignore

--- UnBook/client/components/Nav.js
+++ UnBook/client/components/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav className={"nav d-flex justify-content-end primary bg-primary"}>
       <Link href="/" className="nav-link text-light" data-testid="home-link">Página Inicial</Link>
-      <Link href="/login" className="nav-link active text-light" data-testid="login-link">Entrar</Link>
+      <Link href="/login" className="nav-link active text-light" data-testid="login-link">Login</Link>
       <Link href="/register" className="nav-link active text-light" data-testid="register-link">Cadastrar</Link>
     </nav>
 
