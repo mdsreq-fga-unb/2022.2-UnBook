bbb7d6c3b7b27d396ad7834c87664057e121bb75
gabriel cabral <gabrielbielcabral@gmail.com>
2023-01-29 09:13:59 -0300

    Correção de bug no login
    Co-authored-by: macieljuniormax <macielfcjunior@icloud.com>

--- UnBook/client/pages/login.js
+++ UnBook/client/pages/login.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 import { Modal } from "antd";
 import Link from "next/link";
-import AuthForm from '../components/forms/AuthForm';
+import AuthForm from "../components/forms/AuthForm";
 import { useRouter } from "next/router";
- 
+
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   const router = useRouter();
@@ -16,18 +16,19 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // console.table({ name, email, password, secret });
-        setLoading(true);
-        const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API}/login`,
-       {
-        email,
-        password,
-      });
-      console.log(data);
-      //router.push("/");
+      // console.log(name, email, password, secret);
+      setLoading(true);
+      const { data } = await axios.post(
+        `${process.env.NEXT_PUBLIC_API}/login`,
+        {
+          email,
+          password,
+        }
+      );
+      router.push("/");
     } catch (err) {
-        toast.error(err.response.data);
-        setLoading(false);
+      toast.error(err.response.data);
+      setLoading(false);
     }
   };
 
@@ -35,27 +36,34 @@ const Login = () => {
     <div className="container-fluid">
       <div className="row py-5 text-light bg-default-image">
         <div className="col text-center">
-          <h1>Página de Login</h1>
+          <h1>Login</h1>
         </div>
       </div>
+
       <div className="row py-5">
-        <div className="d-flex justify-content-center">
-          <AuthForm 
-            handleSubmit = {handleSubmit}
-            setEmail = {setEmail}
-            password = {password}
-            setPassword = {setPassword}
-            loading = {loading}
-            page = "login"
+        <div className="col-md-6 offset-md-3">
+          <AuthForm
+            handleSubmit={handleSubmit}
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            loading={loading}
+            page="login"
           />
         </div>
       </div>
 
-        <div className='row'>
-          <div className='col'>
-            <p className='text-center'>Ainda não possui uma conta? {" "}<Link href="/register">Cadastrar</Link></p>
-          </div>
+      <div className="row">
+        <div className="col">
+          <p className="text-center">
+            Ainda não possui cadastro?{" "}
+            <Link href="/register">
+              Cadastrar
+            </Link>
+          </p>
         </div>
+      </div>
     </div>
   );
 };
--- UnBook/server/controllers/auth.js
+++ UnBook/server/controllers/auth.js
@@ -30,7 +30,7 @@ export const register = async (req, res) => {
     }
 };
 
-export const login = async () => {
+export const login = async (req, res) => {
     // verificando se esta recebendo os dados
     //console.log(req.body);
     try{
