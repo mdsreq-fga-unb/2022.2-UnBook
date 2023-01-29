f144f3fdea78bc603c2b31099b0e8dc2197e7cdf
AnaBeatrizMassuh <aaamassuh@gmail.com>
2023-01-29 12:42:54 -0300

    Adicionando marcadores de pagina

--- UnBook/client/components/Nav.js
+++ UnBook/client/components/Nav.js
@@ -1,10 +1,16 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import { UserContext } from "../context";
 import { useRouter } from "next/router";
 
 const Nav = () => {
+  const [current, setCurrent] = useState("");
   const [state, setState] = useContext(UserContext);
+
+  useEffect(() => {
+    process.browser && setCurrent(window.location.pathname);
+  }, [process.browser && window.location.pathname]);
+  
   const router = useRouter();
 
   const logout = () => {  
@@ -17,31 +23,55 @@ const Nav = () => {
     <nav className={"nav d-flex justify-content-end primary bg-primary"}>
       <Link 
         href="/" 
-        className="nav-link text-light logo" 
+        className={`nav-link text-light logo ${current ==="/" && "active"}`} 
         data-testid="home-link">
           UnBook
       </Link>
 
-      <Link 
-        href="/login" 
-        className="nav-link active text-light" 
-        data-testid="login-link">
-          Login
-      </Link>
+      {state !== null ? (
+        <>
+          <Link 
+            href="/user/dashboard" 
+            className={`nav-link text-light ${
+                current === "/user/dashboard" && "active"
+            }`}>
+              {state && state.user && state.user.name}
+          </Link>
 
-      <Link 
-        href="/register" 
-        className="nav-link active text-light" 
-        data-testid="register-link">
-          Cadastrar
-      </Link>
+          <a
+            onClick={logout}
+            className="nav-link text-light" 
+            data-testid="logout-link">
+              Sair
+          </a>
 
-      <a
-        onClick={logout}
-        className="nav-link active text-light" 
-        data-testid="logout-link">
-          Sair
-      </a>
+        </>
+
+      ) : (
+
+        <>
+          <Link 
+            href="/login" 
+            className={`nav-link text-light ${
+              current === "/login" && "active"
+            }`}
+            data-testid="login-link">
+              Login
+          </Link>
+
+          <Link 
+            href="/register" 
+            className={`nav-link text-light ${
+              current === "/register" && "active"
+            }`}
+            data-testid="register-link">
+              Cadastrar
+          </Link>
+        </>
+
+      )}
+
+
     </nav>
 
   );
