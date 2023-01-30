35677c454ca48a47a10829840ba1d1876067ae41
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-30 14:54:13 -0300

    feat: adiciona html para likes e comentários
    Co-authored-by: gabriel cabral <gabrielbielcabral@gmail.com>

--- UnBook/client/components/cards/PostList.js
+++ UnBook/client/components/cards/PostList.js
@@ -2,6 +2,7 @@ import renderHTML from "react-render-html";
 import moment from "moment";
 import { Avatar } from 'antd';
 import PostImage from '../images/PostImage';
+import { HeartOutlined, HeartFilled, CommentOutlined } from "@ant-design/icons"
 
 export const PostList = ({posts}) => {
     return( 
@@ -9,19 +10,28 @@ export const PostList = ({posts}) => {
             {posts && posts.map((post) => <div key={post._id} className="card mb-5">
                 <div className="card-header d-flex justify-content-between align-items-center">
                   <div>
-                    <Avatar size={40} className='mb-2'>
+                    <Avatar size={40}>
                       {post.postedBy.name[0]}  
                     </Avatar>{" "}
-                    <span className="pt-2 ml-3" style={{marginLeft: "0.2rem"}}>{post.postedBy.name}</span>
+                    <span className="ml-3" style={{marginLeft: "0.2rem"}}>{post.postedBy.name}</span>
                   </div>
-                  <span className="pt-2 ml-3">{moment(post.createdAt).fromNow()}</span>
+                  <span className="ml-3">{moment(post.createdAt).fromNow()}</span>
                 </div>
                 <div className="card-body">
                     {renderHTML(post.content)}
                 </div>
                 <div className="card-footer">
                   {post.image && (<PostImage url={post.image.url} />)}
-                  <div className="pt-3">like/ unlike 3 likes 2 comments</div>
+                  <div  className="d-flex align-items-center">
+                    <div className="d-flex align-items-center">
+                      <HeartOutlined className="text-danger h5 mt-2" />
+                      <div className="ps-1">3 likes</div>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <CommentOutlined className="text-danger h5 ps-3 mt-2" />
+                      <div className="ps-1">2 comments</div>
+                    </div>
+                  </div>
                 </div>
             </div>)}
         </>
