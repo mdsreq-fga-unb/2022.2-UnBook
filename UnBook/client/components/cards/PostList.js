a92be0556ffa98c52ebc9ef855abdc7904a41940
Maciel Júnior <macielfcjunior@icloud.com>
2023-01-30 14:11:19 -0300

    feat: lista posts com criados pelo usuário
    Co-authored-by: gabriel cabral <gabrielbielcabral@gmail.com>

--- UnBook/client/components/cards/PostList.js
+++ UnBook/client/components/cards/PostList.js
@@ -1,15 +1,27 @@
+import renderHTML from "react-render-html";
+import moment from "moment";
+import { Avatar } from 'antd';
+import PostImage from '../images/PostImage';
+
 export const PostList = ({posts}) => {
     return( 
         <>
             {posts && posts.map((post) => <div key={post._id} className="card mb-5">
-                <div className="card-header">
-                    <div>image / name/ date</div>
+                <div className="card-header d-flex justify-content-between align-items-center">
+                  <div>
+                    <Avatar size={40} className='mb-2'>
+                      {post.postedBy.name[0]}  
+                    </Avatar>{" "}
+                    <span className="pt-2 ml-3" style={{marginLeft: "0.2rem"}}>{post.postedBy.name}</span>
+                  </div>
+                  <span className="pt-2 ml-3">{moment(post.createdAt).fromNow()}</span>
                 </div>
                 <div className="card-body">
-                    content...
+                    {renderHTML(post.content)}
                 </div>
                 <div className="card-footer">
-                    like/ unlike 3 likes 2 comments
+                  {post.image && (<PostImage url={post.image.url} />)}
+                  <div className="pt-3">like/ unlike 3 likes 2 comments</div>
                 </div>
             </div>)}
         </>
